bb33e9fcbb4f9a0a926b00e37e9b23632257b629
DenisScar <denis.scar@gmail.com>
2025-06-26 13:39:00 -0300

    starts chapter 3

---
+++ cap03/bicycles.py
@@ -0,0 +1,7 @@
+bicicles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicicles)
+print(bicicles[0].title()) # Exibe o primeiro item da lista, com inicial maiúscula
+print(bicicles[-1]) # Exibe o último elemento da lista
+print(bicicles[-2]) # Exibe o penúltimo elemento da lista
+
