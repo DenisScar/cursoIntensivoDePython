005bd5d1d49127793697c68a4b610192df1e2d68
DenisScar <denis.scar@gmail.com>
2025-07-03 14:46:50 -0300

    update chapter 4

---
+++ cap04/ex4.1.py
@@ -0,0 +1,8 @@
+pizzas = ['baiana', 'mussarela', 'marguerita', 'salmone']
+
+for pizza in pizzas:
+    print(F'Gosto muito da pizza {pizza}!')
+print(f"""Pizza apimentada como a {pizzas[0]} é ótima!
+Uma diferente como a {pizzas[3]} vai muito bem!
+Mas uma tradicional, como a {pizzas[1]} ou a {pizzas[2]} é uma delícia!
+Enfim! Adoro pizza!""")
---
+++ cap04/ex4.2.py
@@ -0,0 +1,5 @@
+animals = ['tatu', 'lambari', 'ornitorrinco', 'capivara']
+
+for animal in animals:
+    print(f'Um {animal} seria um ótimo animal de estimação.')
+print('E são todos animais tradicionais da fauna brasileira.')
